150567e355440499ddae44458a32395521d5d7fe
nazozokc <nazozokc@gmail.com>
2026-04-11 21:08:32 +0900

    Update nvim-cmp.lua

--- lua/plugins/completion/nvim-cmp.lua
+++ lua/plugins/completion/nvim-cmp.lua
@@ -100,14 +100,14 @@ return {
 					end
 				end,
 			}),
-
-		sources = {
-			{ name = "copilot", priority = 1050 },
-			{ name = "luasnip", priority = 1000 },
-			{ name = "nvim_lsp", priority = 900 },
-			{ name = "path", priority = 500 },
-			{ name = "buffer", priority = 250 },
-		},
+					
+			sources = {
+				{ name = "copilot", priority = 1050 },
+				{ name = "luasnip", priority = 1000 },
+				{ name = "nvim_lsp", priority = 900 },
+				{ name = "path", priority = 500 },
+				{ name = "buffer", priority = 250 }
+				},
 
 			experimental = {
 				ghost_text = {
