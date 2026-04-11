d0b9400c61341d6fcc6eb0250a2ea6b4b1b5e7b0
nazozokc <nazozokc@gmail.com>
2026-04-11 20:22:42 +0900

    refactor(treesitter): move textobjects config to textobjects plugin

--- lua/plugins/treesitter/nvim-treesitter.lua
+++ lua/plugins/treesitter/nvim-treesitter.lua
@@ -31,60 +31,6 @@ return {
 				enable = true,
 			},
 
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["af"] = { query = "@function.outer", desc = "outer function" },
-						["if"] = { query = "@function.inner", desc = "inner function" },
-						["ac"] = { query = "@class.outer", desc = "outer class" },
-						["ic"] = { query = "@class.inner", desc = "inner class" },
-						["ai"] = { query = "@conditional.outer", desc = "outer conditional" },
-						["ii"] = { query = "@conditional.inner", desc = "inner conditional" },
-						["al"] = { query = "@loop.outer", desc = "outer loop" },
-						["il"] = { query = "@loop.inner", desc = "inner loop" },
-						["aa"] = { query = "@parameter.outer", desc = "outer argument" },
-						["ia"] = { query = "@parameter.inner", desc = "inner argument" },
-						["ab"] = { query = "@block.outer", desc = "outer block" },
-						["ib"] = { query = "@block.inner", desc = "inner block" },
-					},
-				},
-
-				move = {
-					enable = true,
-					set_jumps = true,
-					goto_next_start = {
-						["]f"] = { query = "@function.outer", desc = "Next function start" },
-						["]c"] = { query = "@class.outer", desc = "Next class start" },
-						["]a"] = { query = "@parameter.inner", desc = "Next argument start" },
-					},
-					goto_next_end = {
-						["]F"] = { query = "@function.outer", desc = "Next function end" },
-						["]C"] = { query = "@class.outer", desc = "Next class end" },
-					},
-					goto_previous_start = {
-						["[f"] = { query = "@function.outer", desc = "Prev function start" },
-						["[c"] = { query = "@class.outer", desc = "Prev class start" },
-						["[a"] = { query = "@parameter.inner", desc = "Prev argument start" },
-					},
-					goto_previous_end = {
-						["[F"] = { query = "@function.outer", desc = "Prev function end" },
-						["[C"] = { query = "@class.outer", desc = "Prev class end" },
-					},
-				},
-
-				swap = {
-					enable = true,
-					swap_next = {
-						["<leader>sn"] = { query = "@parameter.inner", desc = "Swap next argument" },
-					},
-					swap_previous = {
-						["<leader>sp"] = { query = "@parameter.inner", desc = "Swap prev argument" },
-					},
-				},
-			},
-
 			incremental_selection = {
 				enable = true,
 				keymaps = {
